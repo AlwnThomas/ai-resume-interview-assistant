c4b3363fdd73559caf5375598729e9e5542f6fa3
Alwin Thomas <alwnthomas@gmail.com>
2026-05-05 20:52:01 +0100

    Hidden Pass FASTapi

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from database import SessionLocal
+from models import User
+from schemas import UserCreate, UserResponse
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
 
 app = FastAPI(
     title="AI Resume & Interview Assistant",
@@ -12,3 +17,41 @@ def root():
 @app.get("/health")
 def health_check():
     return{"status": "ok"}
+
+@app.post("/users")
+def create_user(user: UserCreate):
+
+    db = SessionLocal()
+
+    new_user = User(
+        email=user.email,
+        hashed_password=user.password
+    )
+
+    try:
+        db.add(new_user)
+        db.commit()
+
+    except IntegrityError:
+        db.rollback()
+
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+    
+    finally:
+        db.close()
+
+    return {"message": "User created successfully"}
+
+@app.get("/users", response_model=list[UserResponse])
+def get_users():
+
+    db = SessionLocal()
+
+    users = db.query(User).all()
+
+    db.close()
+
+    return users
